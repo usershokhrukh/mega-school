b7805c5198c76cbc19b3aac6ffa592ba02d8d7fc
Bot1r2010 <batirusman2010@gmail.com>
2026-07-16 19:44:30 +0500

    features/fix-header branches only to fix-header

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,80 +1,8 @@
 import React from 'react'
-import "./results.modules.scss";
-import Image from 'next/image';
 
 const Results = () => {
   return (
-    <div className='results'>
-      <div className="results__box">
-        <h1 className="results__h1">Results</h1>
-        <p className="results__p">Results of our students who took the IELTS exam</p>
-
-        <div className="results__imags-box">
-           <Image
-            src="/images/results/result-1.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-2.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-3.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-4.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-5.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-          <Image
-            src="/images/results/result-6.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-7.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-
-          <Image
-            src="/images/results/result-8.jpg"
-            alt="IELTS Result"
-            width={300}        
-            height={400}       
-            priority         
-          />
-        </div>
-      </div>
-    </div>
+    <div>Results</div>
   )
 }
 
